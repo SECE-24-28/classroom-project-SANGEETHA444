552e3d18cc9910f89a4c64eb49036037354fc70b
sangeetha <sangeetha444rc@gmail.com>
2025-12-05 08:59:21 +0530

    Fourth commit

---
+++ Day4/array.js
@@ -0,0 +1,17 @@
+let arr=["sree","sangee","vp","srinithi"]
+console.log(arr)
+console.log(arr.toString())
+arr.push("ram")
+console.log(arr)
+arr.unshift("hari")
+console.log(arr)
+arr.pop()
+console.log(arr)
+arr.shift()
+console.log(arr)
+
+arr.splice(2,1,"kavi")
+console.log(arr)
+
+let newArr=arr.slice(2,4)
+console.log(newArr)
